6fa886adbf2c2dfcdfb9bdc47ee86fdc4c3c47ba
Benjamin Wolff <benjamin.wolff@digitecgalaxus.ch>
2024-04-01 14:00:37 +0200

    initial commit

---
+++ Casks/neovim-nightly.rb
@@ -0,0 +1,19 @@
+cask "neovim-nightly" do
+  version "nightly"
+  #sha256
+
+  arch arm: "arm64", intel: "x86_64"
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+      verified: "github.com/neovim/"
+  name "Neovim"
+  desc "Vim-fork focused on extensibility and usability"
+  homepage "https://neovim.io"
+
+  #livecheck
+
+  #depends_on
+
+  binary "bin/nvim"
+
+  #zap
+end
