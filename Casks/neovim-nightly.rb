961bea092b59eb0d8dada8dc9f3f403ef67eccbe
Benjamin Wolff <benjamin.wolff@digitecgalaxus.ch>
2024-04-14 15:43:55 +0200

    use latest version instead of commit sha

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,11 +1,11 @@
 cask "neovim-nightly" do
-  cmd = "git ls-remote git@github.com:neovim/neovim.git --tags nightly"
-  commit_sha = `#{cmd}`[0..6]
-  version "nightly-#{commit_sha}"
+  version :latest
+  sha256 :no_check
 
   arch arm: "arm64", intel: "x86_64"
   url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
       verified: "github.com/neovim"
+
   name "Neovim"
   desc "Vim-fork focused on extensibility and usability"
   homepage "https://neovim.io"
